47b325f9e438403e7f0e896a44a194f7e6639abd
gualter <gualterparada@gmail.com>
2023-09-15 21:57:56 +0100

    upgrade modal close button to match design

--- client/src/components/MovieInfo/MovieInfo.tsx
+++ client/src/components/MovieInfo/MovieInfo.tsx
@@ -10,7 +10,7 @@ const divider: SxProps<Theme> = {
   borderColor: "#21B3CF",
   borderWidth: 1.5,
   marginTop: 0,
-  marginBottom: 2
+  marginBottom: 2,
 };
 
 export const MovieInfo = ({ movieId, onClose }: MovieInfoProps) => {
@@ -20,26 +20,37 @@ export const MovieInfo = ({ movieId, onClose }: MovieInfoProps) => {
     <div className="movie_info_container">
       <div className="movie_info_header">
         <span className="title">{movie?.title}</span>
-        <div>
-          <IconButton disableRipple onClick={onClose}>
-            x
+        <div className="close_button" onClick={onClose}>
+          <IconButton disableRipple sx={{padding: 0}}>
+            <img src="close.svg" alt="Close SVG" />
           </IconButton>
-          <span onClick={onClose}>Close</span>
+          <span className="close_text">CLOSE</span>
         </div>
       </div>
-      <Divider sx={divider}/>
-      <InfoSection title="Year" value={new Date(movie.releaseDate).getFullYear().toString()} />
+      <Divider sx={divider} />
+      <InfoSection
+        title="Year"
+        value={new Date(movie.releaseDate).getFullYear().toString()}
+      />
       <InfoSection title="Genre" value={movie.genres.join(", ") || "---"} />
-      <InfoSection title="Description" value={movie.overview} specialClass="description" />
+      <InfoSection
+        title="Description"
+        value={movie.overview}
+        specialClass="description"
+      />
       <Grid container columnSpacing={5}>
         <Grid item xs={12} lg={2}>
-          <InfoSection title="Director" value={movie.director} specialClass="makers" />
+          <InfoSection
+            title="Director"
+            value={movie.director}
+            specialClass="makers"
+          />
         </Grid>
         <Grid item xs={12} lg={10}>
           <InfoSection
             title="Actors"
             value={movie.actors.slice(0, 4).join("  ") || "No actors"}
-            specialClass="makers" 
+            specialClass="makers"
           />
         </Grid>
       </Grid>
@@ -52,5 +63,5 @@ export const MovieInfo = ({ movieId, onClose }: MovieInfoProps) => {
       />
       <InfoSection title="Metascore" value={movie.metascore.toString()} />
     </div>
-  ): null;
+  ) : null;
 };
