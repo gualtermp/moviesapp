6b5a3dd572ed74cd3df352574b7a71e493d94275
gualter <gualterparada@gmail.com>
2023-09-15 21:39:35 +0100

    clean some unused imports

--- server/src/main/java/xpandit/challenge/movies/MovieService.java
+++ server/src/main/java/xpandit/challenge/movies/MovieService.java
@@ -1,7 +1,6 @@
 package xpandit.challenge.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.OrderUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
